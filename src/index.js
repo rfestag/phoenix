57ddb61a56d825ee796aa0ec07f16523d8251a69
Ryan Festag <rfestag@gmail.com>
2018-11-08 18:56:50 -0700

    Removed unused component and unnecessary styling. Working towards pulling css imports out of scss altogether

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
+import "react-reflex/styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
